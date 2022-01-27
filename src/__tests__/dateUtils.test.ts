3121d46d2f2e44dc2c07d8048e2d85f6170ac3be
Daniel <danielc19@ru.is>
2022-01-27 17:00:39 +0000

    Added return type and tests

--- src/__tests__/dateUtils.test.ts
+++ src/__tests__/dateUtils.test.ts
@@ -1,20 +1,39 @@
 import { DATE_UNIT_TYPES } from "../constants";
-import { getCurrentYear, add, isWithinRange } from "../dateUtils";
+import { getCurrentYear, add, isWithinRange, isDateBefore, isSameDay } from "../dateUtils";
 
 describe("Date Utils", () => {
+  describe("getCurrentYear", () => {
+    it('should return current year', () => {
+      const result = getCurrentYear();
+      expect(result).toBe(2022);
+    })
+  })
+
+  describe("add", () => {
+    it('should add to the date correctly', () => {
+      const result = add(new Date('2019-06-11'), 2);
+      expect(result).toStrictEqual(new Date('2019-06-13'));
+    })  
+  })
   
-  it('should return current year', () => {
-    const result = getCurrentYear();
-    expect(result).toBe(2022);
+  describe("isWithinRange", () => {
+    it('should return true if date is within given range, else false', () => {
+      const result = isWithinRange(new Date('2022-07-30'),new Date('2022-01-01'),new Date('2023-01-01'))
+      expect(result).toBe(true);
+    })
   })
 
-  it('should add to the date correctly', () => {
-    const result = add(new Date('2019-06-11'), 2);
-    expect(result).toStrictEqual(new Date('2019-06-13'));
+  describe("isDateBefore", () => {
+    it('should return true if date is before compare date', () => {
+      const result = isDateBefore(new Date('2022-07-30'), new Date('2022-08-30'));
+      expect(result).toBe(true);
+    })
   })
 
-  it('should return true if date is within given range, else false', () => {
-    const result = isWithinRange(new Date('2022-07-30'),new Date('2022-01-01'),new Date('2023-01-01'))
-    expect(result).toBe(true);
+  describe("isSameDay", () => {
+    it('should return true if date is equal', () => {
+      const result = isSameDay(new Date('2022-07-30'), new Date('2022-07-30'));
+      expect(result).toBe(true);
+    })
   })
 });
--- src/dateUtils.ts
+++ src/dateUtils.ts
@@ -1,22 +1,22 @@
 import moment from "moment";
 import { DATE_UNIT_TYPES } from "./constants";
 
-export function getCurrentYear() {
+export function getCurrentYear(): number {
   return moment().year();
 }
 
-export function add(date: Date, number: number, type = DATE_UNIT_TYPES.DAYS) {
+export function add(date: Date, number: number, type = DATE_UNIT_TYPES.DAYS): Date {
   return moment(date).add(number, type).toDate();
 }
 
-export function isWithinRange(date: Date, from: Date, to: Date) {
+export function isWithinRange(date: Date, from: Date, to: Date): boolean {
   return moment(date).isBetween(from, to);
 }
 
-export function isDateBefore(date: Date, compareDate: Date) {
+export function isDateBefore(date: Date, compareDate: Date): boolean {
   return moment(date).isBefore(compareDate);
 }
 
-export function isSameDay(date: Date, compareDate: Date) {
+export function isSameDay(date: Date, compareDate: Date): boolean {
   return moment(date).isSame(compareDate);
 }
